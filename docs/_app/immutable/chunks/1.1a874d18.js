146dd5afdd41029894362d39ab70e7ddf8c7fea4
Morgan Dean <morgandeanhi@gmail.com>
2023-03-26 23:59:25 -0700

    enforce trailing slash?

---
+++ docs/_app/immutable/chunks/1.1a874d18.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.29a2c48c.js";export{t as component};
--- docs/_app/immutable/chunks/1.3f5d6f70.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.ce633b0b.js";export{t as component};
